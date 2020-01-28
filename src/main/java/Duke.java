602609cec4f5b6d175d666a80f061cbcb4cee426
Samantha Goh <>
2020-01-28 19:08:13 +0800

    Commit level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        String divider = "____________________________________________________________";
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -9,10 +12,21 @@ public class Duke {
         String lvlzero = "____________________________________________________________\n"
                 + "Hello! I'm Duke \n"
                 + "What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + "Bye. Hope to see you again soon!\n"
-                +"____________________________________________________________\n";
+                + "____________________________________________________________\n";
         System.out.println (lvlzero);
 
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+        if (line.equals("bye")) {
+            System.out.println(divider);
+            System.out.println("Bye. Hope to see you again soon!");
+            System.out.println(divider);
+        }
+        else {
+            System.out.println(divider);
+            System.out.println(line);
+            System.out.println(divider);
+        }
     }
 }
